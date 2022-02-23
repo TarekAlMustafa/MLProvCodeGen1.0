8e73acdbd33a974308cbe4bbf9f0fa22cbde971c
TarekAlMustafa <talmustafa@web.de>
2022-02-23 10:50:49 +0100

    initial

---
+++ src/index.ts
@@ -0,0 +1,43 @@
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+import { ISettingRegistry } from '@jupyterlab/settingregistry';
+
+import { requestAPI } from './handler';
+
+/**
+ * Initialization data for the extension extension.
+ */
+const plugin: JupyterFrontEndPlugin<void> = {
+  id: 'extension:plugin',
+  autoStart: true,
+  optional: [ISettingRegistry],
+  activate: (app: JupyterFrontEnd, settingRegistry: ISettingRegistry | null) => {
+    console.log('JupyterLab extension extension is activated!');
+
+    if (settingRegistry) {
+      settingRegistry
+        .load(plugin.id)
+        .then(settings => {
+          console.log('extension settings loaded:', settings.composite);
+        })
+        .catch(reason => {
+          console.error('Failed to load settings for extension.', reason);
+        });
+    }
+
+    requestAPI<any>('get_example')
+      .then(data => {
+        console.log(data);
+      })
+      .catch(reason => {
+        console.error(
+          `The extension server extension appears to be missing.\n${reason}`
+        );
+      });
+  }
+};
+
+export default plugin;
