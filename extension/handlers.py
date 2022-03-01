b96a5f4b789518cdbf6310d600120403542e1e87
TarekAlMustafa <talmustafa@web.de>
2022-03-01 09:28:34 +0100

    update

--- extension/handlers.py
+++ extension/handlers.py
@@ -3,6 +3,7 @@ import json
 from jupyter_server.base.handlers import APIHandler
 from jupyter_server.utils import url_path_join
 import tornado
+from . import app
 
 class RouteHandler(APIHandler):
     # The following decorator should be present on all verb methods (head, get, post,
@@ -24,6 +25,62 @@ class RouteHandler2(APIHandler):
         input_data = self.get_json_body()
         data = {"data": "/{}/post_example endpoint!".format(input_data["name"])}
         self.finish(json.dumps(data))
+        
+class RouteHandler3(APIHandler):
+    # The following decorator should be present on all verb methods (head, get, post,
+    # patch, put, delete, options) to ensure only authorized user can request the
+    # Jupyter server
+    @tornado.web.authenticated
+    def post(self):
+        #input_data is an object called objBody
+        input_data = self.get_json_body()
+        reply = app.IC_pytorch(input_data)
+        self.finish(json.dumps(reply))
+
+class RouteHandler4(APIHandler):
+    # The following decorator should be present on all verb methods (head, get, post,
+    # patch, put, delete, options) to ensure only authorized user can request the
+    # Jupyter server
+    @tornado.web.authenticated
+    def post(self):
+        #input_data is an object called objBody
+        input_data = self.get_json_body()
+        reply = app.IC_scikit(input_data)
+        self.finish(json.dumps(reply))
+
+class RouteHandler5(APIHandler):
+    # The following decorator should be present on all verb methods (head, get, post,
+    # patch, put, delete, options) to ensure only authorized user can request the
+    # Jupyter server
+    @tornado.web.authenticated
+    def post(self):
+        #input_data is an object called objBody
+        input_data = self.get_json_body()
+        reply = app.Clustering_scikit(input_data)
+        self.finish(json.dumps(reply))
+
+class RouteHandler6(APIHandler):
+    # The following decorator should be present on all verb methods (head, get, post,
+    # patch, put, delete, options) to ensure only authorized user can request the
+    # Jupyter server
+    @tornado.web.authenticated
+    def post(self):
+        #input_data is an object called objBody
+        input_data = self.get_json_body()
+        reply = app.MS_scikit(input_data)
+        self.finish(json.dumps(reply))        
+
+
+class RouteHandler7(APIHandler):
+    # The following decorator should be present on all verb methods (head, get, post,
+    # patch, put, delete, options) to ensure only authorized user can request the
+    # Jupyter server
+    @tornado.web.authenticated
+    def post(self):
+        #input_data is an object called objBody
+        input_data = self.get_json_body()
+        reply = app.MulticlassClassification(input_data)
+        self.finish(json.dumps(reply))         
     
 def setup_handlers(web_app):
     host_pattern = ".*$"
@@ -31,6 +88,18 @@ def setup_handlers(web_app):
     base_url = web_app.settings["base_url"]
     route_pattern = url_path_join(base_url, "extension", "get_example")
     route_pattern2 = url_path_join(base_url, "extension", "post_example")
+    route_pattern3 = url_path_join(base_url, "extension", "ImageClassification_pytorch")
+    route_pattern4 = url_path_join(base_url, "extension", "ImageClassification_scikit")
+    route_pattern5 = url_path_join(base_url, "extension", "Clustering_scikit")
+    route_pattern6 = url_path_join(base_url, "extension", "ModelSelection_scikit")
+    route_pattern7 = url_path_join(base_url, "extension", "MulticlassClassification")
+    
     handlers = [(route_pattern, RouteHandler),
-                (route_pattern2, RouteHandler2)]
+                (route_pattern2, RouteHandler2),
+                (route_pattern3, RouteHandler3),
+                (route_pattern4, RouteHandler4),
+                (route_pattern5, RouteHandler5),
+                (route_pattern6, RouteHandler6),
+                (route_pattern7, RouteHandler7)]
+                
     web_app.add_handlers(host_pattern, handlers)
